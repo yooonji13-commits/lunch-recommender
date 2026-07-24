96855779c2d97eb7c0f6f537f7a4b4aec47f81af
yooonji13-commits <307008204+yooonji13-commits@users.noreply.github.com>
2026-07-24 13:51:07 +0900

    지도 탭용 카카오 JavaScript 키 설정

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@ const KAKAO_REST_API_KEY = "8fd40ad311379c2b30a12cfa89e08c17";
 
 // 지도 탭에 필요한 카카오 JavaScript 키. REST API 키와 다르며, 반드시
 // 카카오 개발자센터 > 플랫폼 키 > JavaScript 키에 배포 도메인을 등록해야 지도가 뜹니다.
-const KAKAO_JS_KEY = "";
+const KAKAO_JS_KEY = "18d7fce4a73795a7b7fef6e90c1b3ef2";
